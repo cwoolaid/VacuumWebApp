7ef7b95e2887327b48405726ccf434abb5cd4d85
Woohyun Kim <woodenkim92@gmail.com>
2022-11-07 22:20:02 -0600

    11.7.22

--- vacuums/models.py
+++ vacuums/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+
 class Model(models.Model):
-    name = models.CharField(max_length=20)
-    description = models.CharField(max_length=200)
+    TYPES = [
+        (1, 'Cordless'),
+        (2, 'Robot'),
+        (3, 'Wet Dry Vac'),
+        (4, 'Upright'),
+        (5, 'Canister')
+    ]
+    name = models.CharField(max_length=20, blank=False, unique=True)
+    description = models.CharField(max_length=100)
